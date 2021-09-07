904fd5ae02495a1ffdd91963da27790947e733e1
Kingpawn91 <sobers4@yahoo.com>
2021-09-07 16:02:03 -0400

    Updates by Arnold

---
+++ xpostcss.config.js
@@ -0,0 +1,20 @@
+ Volume in drive D is New (500GB)
+ Volume Serial Number is 3C59-2AA6
+
+ Directory of D:\awsprojects\nextjs-blog
+
+09/04/2021  08:00 PM    <DIR>          .
+09/04/2021  08:00 PM    <DIR>          ..
+07/19/2021  02:32 PM               362 .gitignore
+09/04/2021  05:09 PM    <DIR>          .next
+09/04/2021  10:55 AM    <DIR>          components
+09/04/2021  07:59 PM    <DIR>          node_modules
+09/04/2021  07:59 PM           172,236 package-lock.json
+09/04/2021  07:59 PM               369 package.json
+09/04/2021  01:21 PM    <DIR>          pages
+09/04/2021  08:00 PM                 0 postcss.config.js
+09/03/2021  11:03 AM    <DIR>          public
+07/19/2021  02:32 PM                73 README.md
+09/04/2021  05:16 PM    <DIR>          styles
+               5 File(s)        173,040 bytes
+               8 Dir(s)  419,056,009,216 bytes free
